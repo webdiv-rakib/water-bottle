b1332b93efe2af45db76b6f5e5e46a58b10280ff
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-02-07 01:21:10 +0600

    reate Shopping Cart to handle Event with parameters

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,11 +1,12 @@
 import './Bottle.css'
-const Bottle = ({ bottle }) => {
+const Bottle = ({ bottle, handleAddToCard }) => {
     const { name, price, img } = bottle;
     return (
         <div className='bottle-card'>
             <h3>{name}</h3>
             <img className='bottle-img' src={img} alt="" />
-            <h3>Price: {price}</h3>
+            <h3>Price: {price}$</h3>
+            <button onClick={() => handleAddToCard(bottle)}>Purchase</button>
         </div>
     );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -3,20 +3,29 @@ import Bottle from '../Bottle/Bottle';
 import './Bottles.css'
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('bottles.json')
             .then(res => res.json())
             .then(data => setBottles(data))
             .catch(err => console.log(err))
     }, [])
+
+    // add event handler
+    const handleAddToCard = (bottle) => {
+        const newCart = [...cart, bottle];
+        setCart(newCart)
+    }
     return (
         <div>
             <h3>Total Bottles: {bottles.length}</h3>
+            <h3>Card: {cart.length}</h3>
             <div className='bottles-container'>
                 {
                     bottles.map(bottle => <Bottle
                         bottle={bottle}
-                        key={bottle.id}></Bottle>)
+                        key={bottle.id}
+                        handleAddToCard={handleAddToCard}></Bottle>)
                 }
             </div>
         </div>
